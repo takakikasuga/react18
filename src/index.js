9ee9b2eb503d1627d7e44cda025db6c4d30576fa
takakikasuga <takaki.soccer1@gmail.com>
2022-04-01 22:09:55 +0900

    :sparkles: 環境のセットアップ

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// NOTE: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab='home' />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
